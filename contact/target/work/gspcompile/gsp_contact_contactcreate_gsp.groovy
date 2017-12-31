f4ef712e905bfab52d44d1115120a6f2208e5c4b
shishir <shishir4913120@gmail.com>
2017-12-31 10:49:54 +0545

    #first

---
+++ contact/target/work/gspcompile/gsp_contact_contactcreate_gsp.groovy
@@ -0,0 +1,53 @@
+import org.codehaus.groovy.grails.plugins.metadata.GrailsPlugin
+import org.codehaus.groovy.grails.web.pages.GroovyPage
+import org.codehaus.groovy.grails.web.taglib.*
+import org.codehaus.groovy.grails.web.taglib.exceptions.GrailsTagException
+import org.springframework.web.util.*
+import grails.util.GrailsUtil
+
+class gsp_contact_contactcreate_gsp extends GroovyPage {
+public String getGroovyPageFileName() { "/WEB-INF/grails-app/views/contact/create.gsp" }
+public Object run() {
+Writer out = getOut()
+Writer expressionOut = getExpressionOut()
+registerSitemeshPreprocessMode()
+printHtmlPart(0)
+createTagBody(1, {->
+printHtmlPart(1)
+createTagBody(2, {->
+createClosureForHtmlPart(2, 3)
+invokeTag('captureTitle','sitemesh',3,[:],3)
+})
+invokeTag('wrapTitleTag','sitemesh',3,[:],2)
+printHtmlPart(3)
+})
+invokeTag('captureHead','sitemesh',4,[:],1)
+printHtmlPart(3)
+createTagBody(1, {->
+printHtmlPart(4)
+createTagBody(2, {->
+printHtmlPart(5)
+invokeTag('textField','g',8,['name':("name")],-1)
+printHtmlPart(6)
+invokeTag('textField','g',9,['name':("phoneNumber")],-1)
+printHtmlPart(7)
+invokeTag('actionSubmit','g',10,['value':("Save")],-1)
+printHtmlPart(1)
+})
+invokeTag('form','g',11,['action':("save")],2)
+printHtmlPart(3)
+})
+invokeTag('captureBody','sitemesh',12,[:],1)
+printHtmlPart(8)
+}
+public static final Map JSP_TAGS = new HashMap()
+protected void init() {
+	this.jspTags = JSP_TAGS
+}
+public static final String CONTENT_TYPE = 'text/html;charset=UTF-8'
+public static final long LAST_MODIFIED = 1514692388012L
+public static final String EXPRESSION_CODEC = 'html'
+public static final String STATIC_CODEC = 'none'
+public static final String OUT_CODEC = 'html'
+public static final String TAGLIB_CODEC = 'none'
+}
---
+++ contact/target/work/gspcompile/gsp_contact_contactshow_gsp.groovy
@@ -0,0 +1,52 @@
+import org.codehaus.groovy.grails.plugins.metadata.GrailsPlugin
+import org.codehaus.groovy.grails.web.pages.GroovyPage
+import org.codehaus.groovy.grails.web.taglib.*
+import org.codehaus.groovy.grails.web.taglib.exceptions.GrailsTagException
+import org.springframework.web.util.*
+import grails.util.GrailsUtil
+
+class gsp_contact_contactshow_gsp extends GroovyPage {
+public String getGroovyPageFileName() { "/WEB-INF/grails-app/views/contact/show.gsp" }
+public Object run() {
+Writer out = getOut()
+Writer expressionOut = getExpressionOut()
+registerSitemeshPreprocessMode()
+printHtmlPart(0)
+createTagBody(1, {->
+printHtmlPart(1)
+createTagBody(2, {->
+createClosureForHtmlPart(2, 3)
+invokeTag('captureTitle','sitemesh',3,[:],3)
+})
+invokeTag('wrapTitleTag','sitemesh',3,[:],2)
+printHtmlPart(3)
+})
+invokeTag('captureHead','sitemesh',4,[:],1)
+printHtmlPart(3)
+createTagBody(1, {->
+printHtmlPart(4)
+expressionOut.print(contact.name)
+printHtmlPart(5)
+expressionOut.print(contact.phoneNumber)
+printHtmlPart(6)
+createClosureForHtmlPart(7, 2)
+invokeTag('link','g',9,['action':("edit"),'id':(contact.id)],2)
+printHtmlPart(6)
+createClosureForHtmlPart(8, 2)
+invokeTag('link','g',10,['action':("delete"),'id':(contact.id)],2)
+printHtmlPart(3)
+})
+invokeTag('captureBody','sitemesh',11,[:],1)
+printHtmlPart(9)
+}
+public static final Map JSP_TAGS = new HashMap()
+protected void init() {
+	this.jspTags = JSP_TAGS
+}
+public static final String CONTENT_TYPE = 'text/html;charset=UTF-8'
+public static final long LAST_MODIFIED = 1514692468592L
+public static final String EXPRESSION_CODEC = 'html'
+public static final String STATIC_CODEC = 'none'
+public static final String OUT_CODEC = 'html'
+public static final String TAGLIB_CODEC = 'none'
+}
